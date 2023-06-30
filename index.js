8fd76eaffeed3ac3ea38b189d270f33184670e11
Ali <daminovmuhammadali3@gmail.com>
2023-06-30 17:25:04 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+// № 1
+
+
+// let nums = [ 1,2,3,4,5,6,7,8,9,10]
+
+
+
+// let even = nums.filter( (i) =>  i % 2 === 0 )
+
+// let odd = nums.filter( (i) =>  i % 2 !== 0 )
+
+// console.log(even , odd);
+
+
+
+
+
+ // № 2
+
+
+
+
+// let arr = ['ayub','islom','daler','iskandar','shaxzod']
+
+// let ln = arr[0]
+
+// arr.sort(i => {
+//     if (i.length > ln.length) {
+//         ln = i
+//     }
+// })
+
+// console.log(ln);
+
+
+// № 3
+
+
+
+// let index = prompt("index of the name")
+
+// let arr =  ["ayub","islom","daler","iskandar","shaxzod","muhammadali", "axror","abror"]
+
+// let index_name = arr.indexOf(index)
+
+// arr.splice(index-1 ,1)
+
+// console.log(arr);
+
+
+
+
+
+
+// // № 4 
+
+// let cars = [ 20000,16000,40000,30000,35000,60000,13000,15000,45000,110000]
+
+// let from = prompt("from")
+// let up = prompt("up")
+
+// let filtered = cars.filter( i => i >= from && i <= up )
+
+// console.log(filtered);
+
+
+
+// № 5
+
+
+// let arr = [1, 2, false, 'hello', 24, 'world', undefined, null, 'error', 22]
+
+// let numbers = arr.filter( i => typeof(i) === 'number')
+
+// if(numbers.length > 5) {
+//     console.log('good');
+// } else {
+//     console.log('bad');
+// }
+
+
